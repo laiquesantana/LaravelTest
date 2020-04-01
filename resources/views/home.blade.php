9cd8acb5e1747ceaf3998b2876d6c8255ea2dfd8
laique santana <laiquesantana@hotmail.com>
2020-04-01 13:46:45 -0300

    dashboard inicial admin lte, fontawesome, etc

---
+++ resources/views/home.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.master')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <example-component></example-component>
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Dashboard</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+             
+                
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
